8714786d7ca4b41da9efe36d909da886a67d867d
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-07-05 04:24:57 +0300

    Using find() to find elements inside an array

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+function isW(element){
+    return element.result==="W"
+}
+
+function superbowlWin(array){
+const win=array.find(isW)
+ return win?win.year:undefined;
+}
+
+console.log(superbowlWin(record));
